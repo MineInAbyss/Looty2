ef032040f0015bad4d2146284d42cd0e1e8a93dd
Derongan <dgfried@gmail.com>
2019-06-05 20:00:16 -0700

    Empty out the velocity component

--- src/main/java/com/derongan/minecraft/looty/skill/component/effective/VelocityImparting.java
+++ src/main/java/com/derongan/minecraft/looty/skill/component/effective/VelocityImparting.java
@@ -3,11 +3,6 @@ package com.derongan.minecraft.looty.skill.component.effective;
 import com.derongan.minecraft.looty.skill.component.Component;
 
 public class VelocityImparting implements Component {
-    private double strength;
-    private Reference from;
-    private Reference to;
-    private Target target;
-
     public enum Reference {
         INITIATOR,
         TARGET,
@@ -17,31 +12,6 @@ public class VelocityImparting implements Component {
         AWAY
     }
 
-    public enum Target {
-        INITIATOR,
-        TARGET
-    }
-
-    public VelocityImparting(double strength, Reference from, Reference to, Target target) {
-        this.strength = strength;
-        this.from = from;
-        this.to = to;
-        this.target = target;
-    }
-
-    public double getStrength() {
-        return strength;
-    }
-
-    public Reference getFrom() {
-        return from;
-    }
-
-    public Reference getTo() {
-        return to;
-    }
-
-    public Target getTarget() {
-        return target;
+    public VelocityImparting() {
     }
 }
--- src/main/java/com/derongan/minecraft/looty/skill/systems/VelocityImpartingSystem.java
+++ src/main/java/com/derongan/minecraft/looty/skill/systems/VelocityImpartingSystem.java
@@ -3,12 +3,11 @@ package com.derongan.minecraft.looty.skill.systems;
 import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
-import com.badlogic.ashley.systems.IteratingSystem;
 import com.derongan.minecraft.looty.skill.component.effective.VelocityImparting;
 
 import javax.inject.Inject;
 
-public class VelocityImpartingSystem extends IteratingSystem {
+public class VelocityImpartingSystem extends AbstractDelayAwareIteratingSystem {
     public final ComponentMapper<VelocityImparting> velocityImpartingComponentMapper = ComponentMapper.getFor(VelocityImparting.class);
 
     @Inject
@@ -17,7 +16,7 @@ public class VelocityImpartingSystem extends IteratingSystem {
     }
 
     @Override
-    protected void processEntity(Entity entity, float deltaTime) {
-
+    protected void processFilteredEntity(Entity entity, float deltaTime) {
+        
     }
 }
